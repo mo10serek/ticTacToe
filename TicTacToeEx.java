208547177661baef7ed0bb1ecaa1676d68e6cf61
mo10serek <michael.balcerzak@ocsbstudent.ca>
2016-11-10 13:46:40 -0500

    just getting though

--- TicTacToeEx.java
+++ TicTacToeEx.java
@@ -14,37 +14,82 @@ public class TicTacToeEx {
 		
 	static String newline = System.getProperty("line.separator");
 	static Scanner input = new Scanner(System.in);
-
-	static char[] board = new char[] {' ', ' ', ' ', ' ' ,' ' ,' ' ,' ' ,' ' ,' '};
 		
 	static int playerSpot;
 	static int computerSpot;
 		
-	public static void Computer(int counter2){
+	public static char[] Computer(int counter, char[] board) {
 		char letter;
-		
-		if((counter2%2)==0) {
-			letter = 'X';
-		} else {
+
+		if((counter%2)==0) {
 			letter = 'O';
+		} else {
+			letter = 'X';
 		}
 
+		//base case
+		if (board[0] == 'X' && board[1] == 'X' && board[2] == 'X' && board[3] == 'X' && board[4] == 'X' && board[5] == 'X' && board[6] == 'X' && board[7] == 'X' && board[8] == 'X') {
+			return board;
+		} else if (board[0] == letter && board[1] == letter) {
+			board[2] = letter;	
+			return board;
+		} else if (board[1] == letter && board[2] == letter) {
+			board[0] = letter;	
+			return board;
+		} else if (board[3] == letter && board[4] == letter) {
+			board[5] = letter;	
+			return board;
+		} else if (board[4] == letter && board[5] == letter) {
+			board[3] = letter;	
+			return board;
+		} else if (board[6] == letter && board[7] == letter) {
+			board[8] = letter;	
+			return board;
+		} else if (board[7] == letter && board[8] == letter) {
+			board[6] = letter;	
+			return board;
+		} else if (board[0] == letter && board[3] == letter) {
+			board[6] = letter;	
+			return board;
+		} else if (board[6] == letter && board[3] == letter) {
+			board[0] = letter;
+			return board;
+		} else if (board[1] == letter && board[4] == 'X') {
+			board[7] = letter;	
+			return board;
+		} else if (board[7] == letter && board[4] == letter) {
+			board[1] = letter;	
+			return board;
+		} else if (board[2] == letter && board[5] == 'X') {
+			board[8] = letter;	
+			return board;
+		} else if (board[8] == letter && board[5] == letter) {
+			board[2] = letter;	
+			return board;
+		} else if (board[0] == letter && board[4] == 'X') {
+			board[8] = letter;	
+			return board;
+		} else if (board[8] == letter && board[4] == letter) {
+			board[0] = letter;	
+			return board;
+		} else if (board[2] == letter && board[4] == 'X') {
+			board[6] = letter;	
+			return board;
+		} else if (board[6] == letter && board[4] == letter) {
+			board[2] = letter;	
+			return board;
+		}		
 		
-		for (int counter = 0; counter < board.length; counter++){ 
-			if (counter == board.length-1) {
-				
-			} else {
-				board[counter] = letter;
-				
-			}
-
+		for (int counter2 = 0; counter2 < board.length; counter2++){ 			
+			board[counter2] = letter;
+			board = Computer(counter, board);			
 		}
+		return board;
 		
 	}
 	
-	public static void Game(boolean stopGame){
-		List<char[]> posibilities = new ArrayList<char[]>();
-		
+	public static void Game(boolean stopGame, char[] board){
+			
 		System.out.println("which spot you want to put an X on");
 		playerSpot = input.nextInt();
 		while (board[playerSpot - 1] == 'O') {
@@ -61,14 +106,10 @@ public class TicTacToeEx {
 		} 
 		
 		
-		if (stopGame == true) {
-			System.out.println("which spot you want to put an O on");
-			computerSpot = input.nextInt();
-			while (board[computerSpot - 1] == 'X') {
-				System.out.println("this spot has been already selected. Try another spot");
-				computerSpot = input.nextInt();
-			}
-			board[computerSpot - 1] = 'O';
+		if (stopGame == true) {			
+			int counter2 = 2;
+			
+			board = Computer(counter2, board);
 
 			System.out.println(" ||1 " + board[0] + " ||2 " + board[1] + " ||3 " + board[2] + newline + " ||4 " + board[3] + " ||5 " + board[4] + " ||6 " + board[5] + newline + " ||7 " + board[6] + " ||8 " + board[7] + " ||9 " + board[8]);
 	
@@ -78,16 +119,18 @@ public class TicTacToeEx {
 			}
 			
 			if (stopGame == true){
-				Game(stopGame);
+				Game(stopGame, board);
 			}
 		}
 	}
 	
 	public static void main(String[] args) {
+		char[] board = new char[] {' ', ' ', ' ', ' ' ,' ' ,' ' ,' ' ,' ' ,' '};
+		boolean stopGame = true;
+		
 		System.out.println(" ||1 " + board[0] + " ||2 " + board[1] + " ||3 " + board[2] + newline + " ||4 " + board[3] + " ||5 " + board[4] + " ||6 " + board[5] + newline + " ||7 " + board[6] + " ||8 " + board[7] + " ||9 " + board[8]);
 		
-		boolean stopGame = true;
-		Game(stopGame);
+		Game(stopGame, board);
 	}
 }	
 
